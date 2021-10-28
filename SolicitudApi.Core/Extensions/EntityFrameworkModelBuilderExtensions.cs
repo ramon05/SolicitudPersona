cab176c1fe29010881938f5e6adf9bd387819798
Ramon Castillo <ramoncastillo051991@gmail.com>
2021-10-28 03:55:31 -0400

    DbContexts

---
+++ SolicitudApi.Core/Extensions/EntityFrameworkModelBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolicitudApi.Core.BaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace SolicitudApi.Core.Extensions
+{
+    public static class EntityFrameworkModelBuilderExtensions
+    {
+        public static void SetSoftDeleteFilter(this ModelBuilder modelBuilder, Type entityType)
+        {
+            SetSoftDeleteFilterMethod.MakeGenericMethod(entityType).Invoke(null, new object[] { modelBuilder });
+        }
+
+        static readonly MethodInfo SetSoftDeleteFilterMethod = typeof(EntityFrameworkModelBuilderExtensions)
+            .GetMethods(BindingFlags.Public | BindingFlags.Static)
+            .Single(t => t.IsGenericMethod && t.Name == "SetSoftDeleteFilter");
+
+        public static void SetSoftDeleteFilter<TEntity>(this ModelBuilder modelBuilder) where TEntity : class, IBase
+        {
+            modelBuilder.Entity<TEntity>().HasQueryFilter(x => !x.Deleted);
+        }
+    }
+}
---
+++ SolicitudApi.Model/DataContext/SolicitudDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolicitudApi.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolicitudApi.Model.DataContext
+{
+    public partial class SolicitudDbContext : BaseDbContext
+    {
+        public SolicitudDbContext(DbContextOptions<SolicitudDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Document> Documents { get; set; }
+        public virtual DbSet<Person> Persons { get; set; }
+        public virtual DbSet<Team> Teams { get; set; }
+        public DbSet<Solicitude> Solicitudes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
